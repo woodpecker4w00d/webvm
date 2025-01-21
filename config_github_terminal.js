02c870bb1f194ec1a60cb79a6f528d44c01dc68f
Woodpecker3w00d_ <113268712+woodpecker4w00d@users.noreply.github.com>
2025-01-21 11:47:44 -0800

    Update config_github_terminal.js

--- config_github_terminal.js
+++ config_github_terminal.js
@@ -1,5 +1,5 @@
 // The root filesystem location
-export const diskImageUrl = IMAGE_URL;
+export const diskImageUrl = /debian_mini_20230519_5022088024.ext2;
 // The root filesystem backend type
 export const diskImageType = "github";
 // Print an introduction message about the technology
@@ -7,15 +7,15 @@ export const printIntro = true;
 // Is a graphical display needed
 export const needsDisplay = false;
 // Executable full path (Required)
-export const cmd = CMD; // Default: "/bin/bash";
+export const cmd = CMD;  Default: "/bin/bash";
 // Arguments, as an array (Required)
-export const args = ARGS; // Default: ["--login"];
+export const args = ARGS;  Default: ["--login"];
 // Optional extra parameters
 export const opts = {
 	// Environment variables
-	env: ENV, // Default: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
+	env: ENV, Default: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
 	// Current working directory
-	cwd: CWD, // Default: "/home/user",
+	cwd: CWD,  Default: "/home/user",
 	// User id
 	uid: 1000,
 	// Group id
